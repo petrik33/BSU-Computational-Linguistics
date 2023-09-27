01aea3d0571558feb5f199e405ecd4b3499c9232
Timofey Petrikevich <petrik3003@gmail.com>
2023-09-27 12:57:11 +0300

    feat: implement app state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,24 +1,67 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{collections::HashMap, fs::{self, File}, io::{BufReader, BufWriter}, error::Error};
+use std::{
+    collections::HashMap,
+    error::Error,
+    fs::{self, File},
+    io::{BufReader, BufWriter},
+    sync::Mutex,
+};
 
 use serde::Serialize;
+use tauri::State;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+struct Dictionary {
+    mtx: Mutex<HashMap<String, u32>>,
+    path: String,
 }
 
 #[tauri::command]
-fn count_words(path: String) -> Result<HashMap<String, u32>, String> {
-    let content = fs::read_to_string(path).map_err(|e| e.to_string())?;
-    let mut count = HashMap::new();
+fn update_state(path: String, dictionary: State<Dictionary>) -> Result<(), String> {
+    let content = fs::read_to_string(&path).map_err(|e| e.to_string())?;
+    let mut freq_map = dictionary.mtx.lock().unwrap();
     for word in content.split_whitespace() {
-        *count.entry(word.to_lowercase()).or_insert(0) += 1;
+        *freq_map.entry(word.to_lowercase()).or_insert(0) += 1;
     }
-    Ok(count)
+    save_dictionary(&freq_map, &dictionary.path).map_err(|e| e.to_string())?;
+    Ok(())
+}
+
+#[tauri::command]
+fn save_state(dictionary : State<Dictionary>) -> Result<(), String> {
+    let freq_map = dictionary.mtx.lock().unwrap();
+    save_dictionary(&freq_map, &dictionary.path).map_err(|e| e.to_string())?;
+    Ok(())
+}
+
+#[tauri::command]
+fn load_state(dictionary : State<Dictionary>) -> Result<(), String> {
+    let mut freq_map = load_dictionary(&dictionary.path).map_err(|e| e.to_string())?;
+    let mut dict = dictionary.mtx.lock().unwrap();
+    *dict = freq_map;
+    Ok(())
+}
+
+fn main() {
+    let file_path = "./dictionary.json";
+    let mut freq_map = match load_dictionary(file_path) {
+        Ok(d) => d,
+        Err(_) => HashMap::new(), // Fall back to an empty dictionary if there was an error
+    };
+
+    tauri::Builder::default()
+        .manage(Dictionary {
+            mtx: Mutex::from(freq_map),
+            path: String::from(file_path),
+        })
+        .invoke_handler(tauri::generate_handler![
+            update_state,
+            save_state,
+            load_state
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
 
 fn save_dictionary(dict: &HashMap<String, u32>, path: &str) -> Result<(), Box<dyn Error>> {
@@ -34,11 +77,4 @@ fn load_dictionary(path: &str) -> Result<HashMap<String, u32>, Box<dyn Error>> {
     let reader = BufReader::new(file);
     let dict = serde_json::from_reader(reader)?;
     Ok(dict)
-}
-
-fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
 }
