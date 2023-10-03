5bc65ad3eede1b167cc5c14b755af0abbd3f836d
Timofey Petrikevich <petrik3003@gmail.com>
2023-10-03 23:35:18 +0300

    feat: material ui table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from "./app.component";
 import { FrequencyDictionaryComponent } from './frequency-dictionary/frequency-dictionary.component';
 import { AddNewTextComponent } from './add-new-text/add-new-text.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from "@angular/material/sort";
 
 @NgModule({
   declarations: [AppComponent, FrequencyDictionaryComponent, AddNewTextComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/frequency-dictionary/frequency-dictionary.component.ts
+++ src/app/frequency-dictionary/frequency-dictionary.component.ts
@@ -1,31 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { TauriCommunicationService, Dictionary, DictionaryEntry } from '../service/tauri-communication.service';
 import { Observable } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-frequency-dictionary',
   templateUrl: './frequency-dictionary.component.html',
   styleUrls: ['./frequency-dictionary.component.css']
 })
-export class FrequencyDictionaryComponent {
+export class FrequencyDictionaryComponent implements AfterViewInit {
 
   frequencyDictionary$: Observable<Dictionary>;
-  sortedDictionary : DictionaryEntry[] = []
+
+  tableData = new MatTableDataSource<DictionaryEntry>();
+  columnsToDisplay = ['word', 'frequency'];
 
   constructor(private service: TauriCommunicationService) {
     this.frequencyDictionary$ = this.service.frequencyMap$
     this.frequencyDictionary$.subscribe(
       (dictionary) => {
-        this.sortedDictionary = Object.keys(dictionary).map(
+        this.tableData.data = Object.keys(dictionary).map(
           (word) => {
             return {
               frequency: dictionary[word],
               word
             }
           }
-        ).sort((entryA, entryB) => entryB.frequency - entryA.frequency)
+        );
       }
     )
+  }
+
+  @ViewChild(MatPaginator) tablePaginator!: MatPaginator;
+  @ViewChild(MatSort) tableSort!: MatSort;
+
+  ngAfterViewInit() {
+    this.tableData.paginator = this.tablePaginator;
+    this.tableData.sort = this.tableSort;
   }
 
 }
