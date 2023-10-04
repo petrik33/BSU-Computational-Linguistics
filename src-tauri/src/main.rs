55cd132b8706dc3e3858c2be045e91101368dba8
Timofey Petrikevich <petrik3003@gmail.com>
2023-10-04 13:32:20 +0300

    feat: progress state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,39 +1,43 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{collections::HashMap, fs::read_to_string, ops::AddAssign};
+use std::{collections::HashMap, ops::AddAssign, sync::Mutex};
 
 use charabia::Tokenize;
 
+struct ProgressState(Mutex<HashMap<String, f64>>);
+
 #[tauri::command]
-fn make_dictionary_from_file(path: &str) -> Result<HashMap<String, u32>, String> {
-    read_to_string(path)
-        .map(|text| make_dictionary(&text))
-        .map_err(|info| info.to_string())
+async fn get_progress(command: &str, state: tauri::State<'_, ProgressState>) -> Result<f64, String> {
+    let progress_map = state.0.lock().unwrap();
+    match progress_map.get(command) {
+        Some(progress) => Ok(*progress),
+        None => Err(String::from("No such command is active")),
+    }
 }
 
 #[tauri::command]
-fn make_dictionary(text: &str) -> HashMap<String, u32> {
+async fn make_dictionary(
+    text: &str,
+    state: tauri::State<'_, ProgressState>,
+) -> Result<HashMap<String, u32>, String> {
     let mut dictionary: HashMap<String, u32> = HashMap::new();
     let tokens = text.tokenize();
+
     for token in tokens {
         if !token.is_word() {
             continue;
         }
 
-        let word = token.lemma().to_string();
-
-        dictionary.entry(word).or_insert(0).add_assign(1);
+        dictionary.entry(token.lemma().to_string()).or_insert(0).add_assign(1);
     }
-    dictionary
+    Ok(dictionary)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            make_dictionary_from_file,
-            make_dictionary
-        ])
+        .manage(ProgressState(Mutex::from(HashMap::new())))
+        .invoke_handler(tauri::generate_handler![make_dictionary, get_progress])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
