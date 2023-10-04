50b90449ff8cb7e32330de2c6ceb6bdb2a85b89c
Timofey Petrikevich <petrik3003@gmail.com>
2023-10-04 13:31:55 +0300

    feat: layout

--- src/app/service/tauri-communication.service.ts
+++ src/app/service/tauri-communication.service.ts
@@ -17,15 +17,22 @@ export interface DictionaryEntry {
 export class TauriCommunicationService {
 
   private frequencyMapSubject: BehaviorSubject<Dictionary> = new BehaviorSubject<Dictionary>({});
+  private taskInProgressSubject = new BehaviorSubject<boolean>(false);
 
   constructor() {}
 
   async addText(text : string) {
+    this.taskInProgressSubject.next(true);
     let dict = await invoke<Record<string, number>>('make_dictionary', { text })
     for (let key in dict) {
       this.addWord(key, dict[key]);
     }
     this.frequencyMapSubject.next(this.frequencyMap);
+    this.taskInProgressSubject.next(false);
+  }
+
+  async getCommandProgress(command : string) {
+    return await invoke<number>('get_progress', { command });
   }
 
   removeWord(word : string) {
@@ -48,6 +55,10 @@ export class TauriCommunicationService {
 
   get frequencyMap$(): Observable<Dictionary> {
     return this.frequencyMapSubject.asObservable();
+  }
+
+  get taskInProgress$(): Observable<boolean> {
+    return this.taskInProgressSubject.asObservable();
   }
 
   private get frequencyMap(): Dictionary {
