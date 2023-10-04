dfed44b23089305729a8fc7393fbf7f621bcda19
Timofey Petrikevich <petrik3003@gmail.com>
2023-10-04 13:31:42 +0300

    feat: loading

--- src/app/add-new-text/add-new-text.component.ts
+++ src/app/add-new-text/add-new-text.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { TauriCommunicationService } from '../service/tauri-communication.service';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-add-new-text',
@@ -8,17 +10,36 @@ import { TauriCommunicationService } from '../service/tauri-communication.servic
 })
 export class AddNewTextComponent {
 
-  constructor(private tauriService: TauriCommunicationService) { }
-
-  async uploadFile(event: Event) {
-    const inputElement = event.target as HTMLInputElement;
-    const file = inputElement?.files?.[0];
+  taskInProgress$ : Observable<boolean> = of(false);
+  file : File | undefined;
   
-    if (file) {
-      let text = await file.text()
+  constructor(private tauriService: TauriCommunicationService) {
+    this.taskInProgress$ = tauriService.taskInProgress$
+  }
+
+  @ViewChild(MatProgressBar) progressBar!: MatProgressBar;
+
+  async uploadFile() {
+    if (this.file) {
+      let text = await this.file.text()
       this.tauriService.addText(text)
         .then(() => console.log('File uploaded successfully'))
         .catch((reason) => console.log(`Error while uploading file: ${reason}`))
+      // this.trackProgress()
     }
-  }  
+  }
+
+  onFileChanged(event : Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.file = inputElement?.files?.[0];
+  }
+
+  // async trackProgress() {
+  //   let interval = setInterval(async () => {
+  //     this.progressBar.value = await this.tauriService.getCommandProgress('make_dictionary') * 100;
+  //     if (this.progressBar.value >= 100) {
+  //       clearInterval(interval);
+  //     }
+  //   }, 100);
+  // }
 }
