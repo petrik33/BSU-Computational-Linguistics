cb2c131998f036d08fa9b2024c3a0ecdd021f5fb
Timofey Petrikevich <petrik3003@gmail.com>
2023-10-17 14:14:40 +0300

    feat: -external lib for parsing

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,6 @@
 
 use std::{collections::HashMap, ops::AddAssign, sync::Mutex};
 
-use charabia::{Tokenize};
-
 struct ProgressState(Mutex<HashMap<String, f64>>);
 
 #[tauri::command]
@@ -16,24 +14,31 @@ async fn get_progress(command: &str, state: tauri::State<'_, ProgressState>) -> 
     }
 }
 
+fn is_word(token: &str) -> bool {
+    token.chars().all(|c| c.is_alphabetic())
+}
+
 #[tauri::command]
 async fn make_dictionary(
     text: &str,
 ) -> Result<HashMap<String, u32>, String> {
     let mut dictionary: HashMap<String, u32> = HashMap::new();
-    let tokens = text.tokenize();
+
+    let tokens = text.split_whitespace();
 
     for token in tokens {
-        if !token.is_word() {
+        if !is_word(token) {
             continue;
         }
 
-        if token.lemma().parse::<f64>().is_ok() {
+        if token.parse::<f64>().is_ok() {
             continue;
         }
 
-        dictionary.entry(token.lemma().to_string()).or_insert(0).add_assign(1);
+        let lemma = token.to_lowercase();
+        *dictionary.entry(lemma).or_insert(0) += 1;
     }
+
     Ok(dictionary)
 }
 
